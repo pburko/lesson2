fcef055015380c1aa6e8efebcc8cb5b1b001d0d7
pburko <burko@mail.ru>
2019-04-05 19:40:11 +0300

    v1.0

---
+++ src/by/belhard/j2/lesson2/Main.java
@@ -0,0 +1,11 @@
+package by.belhard.j2.lesson2;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int x;
+        x=5;
+        System.out.println(x);
+    }
+}
